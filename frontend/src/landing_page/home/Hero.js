ed31fdbf1fe04451febdd4c527f86d1695c10e84
Rahul Kumar <krahulr111@gmail.com>
2025-09-19 19:25:49 +0530

    Design whole Home Page

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -9,7 +9,6 @@ function Hero() {
                 <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
                 <button className="p-2 btn btn-primary fs-5 mb-5" style={{width:"20%", margin: "0 auto"}}>Signup Now</button>
             </div>
-
         </div>
     );
 }
